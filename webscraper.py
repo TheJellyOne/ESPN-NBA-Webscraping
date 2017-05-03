632a26febc67a7928328a1ef09a41b84080f9674
Moritz Droste <Moritz Droste>
2017-05-02 20:35:54 -0700

    made some progress

--- webscraper.py
+++ webscraper.py
@@ -5,5 +5,25 @@
 """
 
 from bs4 import BeautifulSoup
-import urllib
+import requests
 
+url = requests.get('http://www.espn.com/nba/statistics/player/_/stat/scoring/sort/points/qualified/false')
+
+# print(url.text) 
+
+soup = BeautifulSoup(url.text, "lxml")
+players = []
+pages = []
+pretty_soup = soup.prettify()
+
+        
+for j in soup.find_all('a'):
+    if "www.espn.com/nba/statistics/player/_/stat/scoring/sort/points/qualified/false/count" in j.get('href'):
+        pages.append(j.get('href'))
+        
+for i in soup.find_all('a'):
+    if "www.espn.com/nba/player/_/id" in i.get('href'):
+        players.append(i.get('href'))
+        
+        
+print(len(pages))
