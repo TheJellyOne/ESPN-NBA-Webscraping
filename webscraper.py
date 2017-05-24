877d09b15ef7b77bbd3fcbf1e6e3065cc12f2c1e
Moritz Droste <Moritz Droste>
2017-05-23 20:22:42 -0700

    ran the whole set of players - massive csv file now

--- webscraper.py
+++ webscraper.py
@@ -35,7 +35,7 @@ for link in pageList:
             statPage = i.get('href')[:30] + "/stats" + i.get('href')[30:]
             players.append(statPage)
 
-testset = players[:10]
+testset = players
 
 data = []
 for player in testset:
